25a7b0e4b691fe4ef098fcd5fd2f0a202b15c514
Jhanluck Onofre <jonofre@znerg.com>
2021-08-18 02:32:41 -0500

    Created the method fetch

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,44 @@
-import { createStore } from 'vuex'
-
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
-    
+    commit: {
+      id: "",
+      message: "",
+      autor: "",
+    },
+    commits: [],
   },
   mutations: {
-  
+    load(state, payload) {
+      state.commits = payload;
+    },
   },
-  
- 
-})
-
+  actions: {
+    async loadCommits({ commit }) {
+      try {
+        const rest = await fetch(
+          "https://api.github.com/repos/AndresOnofre23/Angular-Bases/commits"
+        );
+        const data = await rest.json();
+        console.log(data);
+        const array2 = [];
+        data.forEach((element) => {
+          const obj = {
+            id: "",
+            message: "",
+            autor: "",
+          };
+          obj.id=element.commit.tree.sha;
+          obj.message=element.commit.message;
+          obj.autor=element.commit.author.name;
+          array2.push(obj);
+        });
+        commit('load',array2);
+        console.log(array2);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+});
